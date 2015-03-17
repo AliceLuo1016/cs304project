192d8c1e9a7965158f79cb0270148f96b74480f5
Alice Luo <alice.luoqian@gmail.com>
2015-03-17 13:42:59 -0700

    Initial commit.

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+  <title>Zoo Database</title>
+  <h1> ZOO DATABASE </h1>
+</head>
+
+<body>
+
+  <div class="content">
+			<div style="width:100%; text-align:center; padding-top:50px; padding-bottom:30px;">
+				<h2 style="font-size: 1.4em;"> WELCOME! <br /> <br /> THIS IS OUR ZOO DATABASE. </h2>
+			</div>
+  </div>
+
+</body>
+
+</html>
