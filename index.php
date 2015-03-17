1ebc9aeca0664a752bb5af96c03e4a48994a4b3d
unknown <heuvenlam@gmail.com>
2015-03-17 16:38:19 -0700

    Added ext. css stylesheet & implemented dummy menu

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <html>
 <head>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
   <title>Zoo Database</title>
-  <h1> ZOO DATABASE </h1>
+  <h1>PseudoZoo</h1>
 </head>
 
 <body>
 
+<nav id="primary_nav_wrap">
+<ul>
+  <li><a href="#">Home</a>
+  <li><a href="#">Data Tables</a>
+    <ul>
+      <li><a href="#">Animals</a></li>
+      <li><a href="#">Habitats</a></li>
+      <li><a href="#">Performances</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Customer Service</a>
+    <ul>
+      <li><a href="#">Member Application</a></li>
+      <li><a href="#">Leave a Comment</a></li>
+    </ul>
+  </li>
+  <li><a href="#">Contact Us</a></li>
+</ul>
+</nav>
+
   <div class="content">
-			<div style="width:100%; text-align:center; padding-top:50px; padding-bottom:30px;">
-				<h2 style="font-size: 1.4em;"> WELCOME! <br /> <br /> THIS IS OUR ZOO DATABASE. </h2>
-			</div>
+		<div>
+			<h2> WELCOME! <br /> <br /> THIS IS OUR ZOO DATABASE. </h2>
+		</div>
   </div>
 
 </body>
