1f3011790ed1cd38c22122109d370c7fa27a6216
Alice Luo <alice.luoqian@gmail.com>
2015-03-24 16:11:42 -0700

    fix the problem of last commit

--- index.php
+++ index.php
@@ -33,10 +33,6 @@
               onMouseOut="this.style.backgroundColor='#2E8B57'">Sign In</a>
             </li>
 
-            <li>
-              <a href="register.php" style="color: #fff;" onMouseOver="this.style.backgroundColor='#3CB371'"
-              onMouseOut="this.style.backgroundColor='#2E8B57'">Register</a>
-            </li>
           </ul>
 				</ul>
         </nav>
@@ -49,7 +45,17 @@
   <br>
   <br>
 
-  <div class="list-group pull-right col-md-3">
+  <div class="row">
+  <div class="col-xs-12 col-md-8">
+      <div class="welcomeMsg">
+      <div style="width:100%; text-align:center; padding-top:50px; padding-bottom:30px;">
+        <h2 style="font-size: 1.4em;"> WELCOME! <br /> <br /> THIS IS OUR ZOO DATABASE. </h2>
+      </div>
+  </div>
+</div>
+
+  <div class="col-md-4">
+  <div class="list-group" style="width:80%">
   <br>
   <label> Display </label>
   <a href="animals.php" class="list-group-item">Animals</a>
@@ -60,13 +66,9 @@
   <a href="membership.php" class="list-group-item">Membership Application</a>
   <a href="comments.php" class="list-group-item">Comments</a>
   </div>
-
-
-  <div class="welcomeMsg">
-			<div style="width:100%; text-align:center; padding-top:50px; padding-bottom:30px;">
-				<h2 style="font-size: 1.4em;"> WELCOME! <br /> <br /> THIS IS OUR ZOO DATABASE. </h2>
-			</div>
   </div>
+</div>
+
 
 
 </body>
