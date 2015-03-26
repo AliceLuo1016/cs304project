901da87f20c913db272ff0df5cfef71830640b8b
unknown <heuvenlam@gmail.com>
2015-03-26 05:03:04 -0700

    Count Query- index.php member count

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
-<html>
 
+<?php 
+include "connectfunc.php";
+session_start();
+
+$tup;
+function update_mem_count($condb) {
+	$query = "SELECT COUNT(*) FROM member";
+	$result = $condb->query($query);
+	global $tup;
+	
+	if($result->num_rows > 0) {
+		$rowone = $result->fetch_array(MYSQLI_NUM);
+		$tup = $rowone[0];
+		$result->free();
+	}
+	
+}
+
+$condb = conn_db();
+update_mem_count($condb);
+if (isset($_POST["update_count"])) {
+    update_mem_count($condb);
+}
+dconn_db($condb);
+?>
+
+<html>
 
 <head>
   <title>PseudoZoo</title>
@@ -33,7 +59,6 @@
               <a href="login.php" style="color: #fff;" onMouseOver="this.style.backgroundColor='#3CB371'"
               onMouseOut="this.style.backgroundColor='#2E8B57'">Sign In</a>
             </li>
-
           </ul>
 				</ul>
         </nav>
@@ -51,7 +76,14 @@
       <div class="welcomeMsg">
       <div style="width:100%; text-align:center; padding-top:50px; padding-bottom:30px;">
         <h2 style="font-size: 1.4em;"> WELCOME! <br /> <br /> THIS IS OUR ZOO DATABASE. </h2>
-      </div>
+      <form>
+			Member Count: <input type = "submit" value = "Update" name = "update_count">
+			<?php 
+			  global $tup;
+			  echo $tup;
+			?>
+			</form>
+	  </div>
   </div>
 </div>
 
@@ -74,9 +106,6 @@
 </div>
 
 
-
 </body>
 
 </html>
-
-
