dc74f44c5a73f87bcb12493606eef1d8e569929d
unknown <heuvenlam@gmail.com>
2015-03-26 03:37:34 -0700

    Join Query- find trainer name based on perf name

--- performance.php
+++ performance.php
@@ -39,8 +39,35 @@ function print_performance($condb) {
 	}
 }
 
+function find_trainer_from_name($condb) {
+	if ($_POST["pname"]) {
+	    $aniname = $_POST["pname"];
+	    $aniname = mysqli_real_escape_string($condb, $aniname);
+	   
+	    $query = "SELECT Name FROM trainer t, direct_performance d 
+				WHERE t.Empl_ID = d.EmpID
+				AND d.pName = '" . "$aniname" . "'";
+				
+		$result = $condb->query($query);
+		if($result->num_rows > 0) {
+			while ($tup = $result->fetch_assoc()) {
+			  echo $tup["Name"] . "<br>";
+			}
+		} else echo "No results, sorry" . "<br>";
+	}else echo "Please enter a performance name!" . "<br>";
+}
+
 $condb = conn_db();
 print_performance($condb);
+if(isset($_POST["submitone"])) {
+	find_trainer_from_name($condb);
+}
 dconn_db($condb);
 
 ?>
+
+Find out who directs these performances!
+  <form action="<?php $_PHP_SELF ?>" method = "POST">
+  Enter a performance name: <input type = "text" name = "pname">
+  <input type = "submit" name = "submitone" />
+  </form>
