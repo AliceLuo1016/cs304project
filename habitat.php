d0824056e1ef4f495038def5e02869387778d2a1
unknown <heuvenlam@gmail.com>
2015-03-27 01:31:53 -0700

    Added habitat query

--- habitat.php
+++ habitat.php
@@ -44,11 +44,38 @@ function print_habitats($condb) {
 	}
 }
 
+function find_animal_from_habitat($condb) {
+	if ($_POST["hname"]) {
+	    $habname = $_POST["hname"];
+	    $habname = mysqli_real_escape_string($condb, $habname);
+	   
+	    $query = "SELECT aName FROM animal a, habitat h WHERE 
+				a.hName = h.hName AND h.hName = '". "$habname". "'";
+				
+		$result = $condb->query($query);
+		if($result->num_rows > 0) {
+			while ($tup = $result->fetch_assoc()) {
+			  echo $tup["aName"] . "<br>";
+			}
+			$result->free();
+		} else echo "No results, sorry" . "<br>";
+	}else echo "Please enter a habitat name!" . "<br>";
+}
+
 $condb = conn_db();
 ?>
 <h1> Habitat Table </h1>
 <?php
 print_habitats($condb);
+if (isset($_POST["submitone"])) {
+	find_animal_from_habitat($condb);
+}
 dconn_db($condb);
 
 ?>
+
+Find out which animals live in the habitats!
+  <form action="<?php $_PHP_SELF ?>" method = "POST">
+  Enter a habitat name: <input type = "text" name = "hname">
+  <input type = "submit" name = "submitone" />
+  </form> <br>
